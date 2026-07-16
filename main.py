c4fd0b615b9b9df094292b1c61037b2022b0e2d4
vh8432348-ship-it <vh8432348@gmail.com>
2026-07-16 21:23:32 +0300

    gfdgdg

--- main.py
+++ main.py
@@ -1,49 +1,36 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from ultralytics import YOLO
 
-image = cv2.imread("data/lesson1/Lenna.png")
+model = YOLO("data/lesson_seg/crop-seg.pt")
+
+image = cv2.imread("data/lesson_seg/crop3.jpg")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-mask1 = cv2.imread("data/lesson1/mask1.png", cv2.IMREAD_GRAYSCALE)
-mask2 = cv2.imread("data/lesson1/mask2.png", cv2.IMREAD_GRAYSCALE)
+results = model(image)
 
+result = results[0]
 
-mask_or = cv2.bitwise_or(mask1, mask2)
+for i in range(len(result.boxes)):
+    class_id = int(result.boxes.cls[i])
+    name = result.names[class_id]
 
-plt.figure(figsize=(5,5))
-plt.imshow(mask_or, cmap="gray")
-plt.title("Об'єднана маска")
-plt.axis("off")
-plt.show()
+    mask = result.masks.data[i].cpu().numpy()
 
-mask1_bool = mask1.astype(bool)
-mask2_bool = mask2.astype(bool)
-mask_or_bool = mask_or.astype(bool)
+    mask = cv2.resize(
+        mask,
+        (image.shape[1], image.shape[0]),
+        interpolation=cv2.INTER_NEAREST
+    )
 
-img_mask1 = np.zeros_like(image)
-img_mask2 = np.zeros_like(image)
-img_mask_or = np.zeros_like(image)
+    mask = mask > 0.5
 
-img_mask1[mask1_bool] = image[mask1_bool]
-img_mask2[mask2_bool] = image[mask2_bool]
-img_mask_or[mask_or_bool] = image[mask_or_bool]
+    plant = np.full_like(image, 255)
+    plant[mask] = image[mask]
 
-plt.figure(figsize=(15,5))
-
-plt.subplot(1,3,1)
-plt.imshow(img_mask1)
-plt.title("Mask1")
-plt.axis("off")
-
-plt.subplot(1,3,2)
-plt.imshow(img_mask2)
-plt.title("Mask2")
-plt.axis("off")
-
-plt.subplot(1,3,3)
-plt.imshow(img_mask_or)
-plt.title("Mask1 OR Mask2")
-plt.axis("off")
-
-plt.show()
+    plt.figure(figsize=(5, 5))
+    plt.imshow(plant)
+    plt.title(name)
+    plt.axis("off")
+    plt.show()
