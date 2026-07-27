f9f9b21d34119ea2d0c7a3f1d616d52da82fe1ba
vh8432348-ship-it <vh8432348@gmail.com>
2026-07-27 15:27:04 +0300

    fgdg

--- main.py
+++ main.py
@@ -2,44 +2,99 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread("data/lesson2/darken.png")
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#Завдання 1
 
-hsv_equalized = hsv.copy()
-hsv_equalized[:, :, 2] = cv2.equalizeHist(hsv_equalized[:, :, 2])
+img = cv2.imread("data/lesson3/sonet.png")
 
-result_equalized = cv2.cvtColor(hsv_equalized, cv2.COLOR_HSV2RGB)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-hsv_bright = hsv.copy()
+blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-value = hsv_bright[:, :, 2].astype(np.float32)
-value = value * 1.3
-value = np.clip(value, 0, 255)
-value = value.astype(np.uint8)
+binary = cv2.adaptiveThreshold(
+    blur,
+    255,
+    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv2.THRESH_BINARY,
+    11,
+    2
+)
 
-hsv_bright[:, :, 2] = value
+kernel = np.ones((3, 3), np.uint8)
 
-result_bright = cv2.cvtColor(hsv_bright, cv2.COLOR_HSV2RGB)
+clean = cv2.morphologyEx(
+    binary,
+    cv2.MORPH_OPEN,
+    kernel,
+    iterations=1
+)
 
-original = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-plt.figure(figsize=(15,5))
+plt.figure(figsize=(12, 4))
 
-plt.subplot(1,3,1)
-plt.imshow(original)
-plt.title("Оригінал")
-plt.axis("off")
+plt.subplot(1, 4, 1)
+plt.imshow(gray, cmap="gray")
+plt.title("Gray")
 
-plt.subplot(1,3,2)
-plt.imshow(result_equalized)
-plt.title("Вирівнювання гістограми")
-plt.axis("off")
+plt.subplot(1, 4, 2)
+plt.imshow(blur, cmap="gray")
+plt.title("Blur")
 
-plt.subplot(1,3,3)
-plt.imshow(result_bright)
-plt.title("Value +30%")
-plt.axis("off")
+plt.subplot(1, 4, 3)
+plt.imshow(binary, cmap="gray")
+plt.title("Adaptive")
 
-plt.tight_layout()
+plt.subplot(1, 4, 4)
+plt.imshow(clean, cmap="gray")
+plt.title("Clean")
+
+plt.show()
+
+
+
+#Завдання 2
+
+img = cv2.imread("data\lesson3\sonet_noised.png")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+blur = cv2.medianBlur(gray, 5)
+
+binary = cv2.adaptiveThreshold(
+    blur,
+    255,
+    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv2.THRESH_BINARY,
+    11,
+    2
+)
+
+kernel = np.ones((3, 3), np.uint8)
+
+clean = cv2.morphologyEx(
+    binary,
+    cv2.MORPH_OPEN,
+    kernel,
+    iterations=2
+)
+
+
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 4, 1)
+plt.imshow(gray, cmap="gray")
+plt.title("Gray noised")
+
+plt.subplot(1, 4, 2)
+plt.imshow(blur, cmap="gray")
+plt.title("Median blur")
+
+plt.subplot(1, 4, 3)
+plt.imshow(binary, cmap="gray")
+plt.title("Adaptive")
+
+plt.subplot(1, 4, 4)
+plt.imshow(clean, cmap="gray")
+plt.title("Clean result")
+
 plt.show()
