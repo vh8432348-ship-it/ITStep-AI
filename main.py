e4bae608b5745b5ef90002bb5fd7cdd1d7ddb3ad
vh8432348-ship-it <vh8432348@gmail.com>
2026-08-27 21:19:16 +0300

    fgdg

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 import dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 import os
+from langchain_core.messages import trim_messages
 
 dotenv.load_dotenv()
 
@@ -19,72 +21,135 @@ llm = ChatGoogleGenerativeAI(
 )
 
 
-class NameBooks(BaseModel):
-    genre: str = Field(description="Жанр книги")
+with open("data/lesson9/return_policy.txt", "r", encoding="utf-8") as file:
+    rules = file.read()
 
 
-parser = PydanticOutputParser(pydantic_object=NameBooks)
+# Інструкція для моделі
+messages = [
+    SystemMessage(content=f"""
+Ти чат-бот, який відповідає тільки на питання
+стосовно умов повернення товару.
 
-instructions = parser.get_format_instructions()
+Використовуй інформацію тільки з наведених правил:
 
-prompt = ChatPromptTemplate.from_messages([
-    (
-        "system",
-        """
-        Ти визначаєш жанр книги.
+{rules}
 
-        {instructions}
-        """
-    ),
-    (
-        "human",
-        "Визнач жанр книги: {book}"
-    )
-])
+Якщо користувач запитує щось, що не стосується
+умов повернення товару або на це немає інформації
+у правилах, відповідай:
 
-chain = prompt | llm | parser
+"Немає інформації з цього питання."
+""")
+]
 
 
-result = chain.invoke({
-    "book": "1984",
-    "instructions": instructions
-})
-
-print(result)
-print(result.genre)
-
-class RecommendedBooks(BaseModel):
-    books: list[str] = Field(
-        description="Список схожих книг"
-    )
-
-parser_recommendations = PydanticOutputParser(
-    pydantic_object=RecommendedBooks
+trimmer = trim_messages(
+    max_tokens=5,
+    strategy="last",
+    token_counter=len,
+    start_on="human",
+    end_on="human",
+    include_system=True
 )
 
-instructions_recommendations = (
-    parser_recommendations.get_format_instructions()
-)
+#
+# while True:
+#
+#     user_input = input("Ви: ")
+#
+#     if user_input.lower() == "exit":
+#         break
+#
+#     messages.append(
+#         HumanMessage(content=user_input)
+#     )
+#
+#     trimmed_messages = trimmer.invoke(messages)
+#
+#     response = llm.invoke(trimmed_messages)
+#
+#     print("Бот:", response.content[0]["text"])
+#
+#     messages.append(
+#         AIMessage(content=response.content)
+#     )
+#
 
-prompt_recommendations = ChatPromptTemplate.from_messages([
-    (
-        "system",
-        """
-        Ти рекомендуєш книги.
+messages = [
+    SystemMessage(content="""
+Ти допомагаєш користувачу вивчати англійську мову.
 
-        {instructions}
-        """
-    ),
-    (
-        "human",
-        """
-        Назва книги: {book}
-        Жанр книги: {genre}
+Твої правила:
 
-        Підбери 5 схожих книг.
-        Частина повинна бути того самого жанру,
-        частина може бути іншого жанру,
-        але схожа за сюжетом, тематикою або атмосферою.
-        """
+1. Якщо користувач просить перекласти англійське слово
+або коротку фразу:
+- дай переклад;
+- наведи приклад використання цього слова або фрази
+  в англійському реченні;
+- переклади приклад українською.
+
+2. Якщо користувач просить перекласти ціле речення:
+- спочатку дай переклад речення;
+- потім поясни граматику речення;
+- зверни увагу на граматичні конструкції,
+  час, порядок слів, утворення питання тощо.
+
+Відповідай українською мовою.
+"""),
+
+    HumanMessage(content="Переклади слово beautiful"),
+
+    AIMessage(content="""
+Переклад: beautiful — красивий, прекрасний.
+
+Приклад:
+She has a beautiful smile.
+— У неї прекрасна посмішка.
+"""),
+
+    # Приклад 2
+    HumanMessage(content="Переклади речення There is a book on the table."),
+
+    AIMessage(content="""
+Переклад: На столі є книга.
+
+Граматика:
+There is використовується, коли ми говоримо про
+наявність одного предмета або особи.
+
+There is + однина:
+There is a book on the table.
+
+Для множини використовується There are:
+There are two books on the table.
+
+Структура:
+There is/are + предмет + місце.
+""")
+]
+
+
+while True:
+
+    user_input = input("Ви: ")
+
+    if user_input.lower() == "exit":
+        break
+
+    messages.append(
+        HumanMessage(content=user_input)
     )
-])
+
+    response = llm.invoke(messages)
+
+    if isinstance(response.content, list):
+        answer = response.content[0]["text"]
+    else:
+        answer = response.content
+
+    print("Бот:", answer)
+
+    messages.append(
+        AIMessage(content=answer)
+    )
