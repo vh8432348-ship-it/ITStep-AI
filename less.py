df3d11a6dfbf76751f4197021815ae9d203b21b2
vh8432348-ship-it <vh8432348@gmail.com>
2026-08-27 11:42:42 +0300

    gdfdh

--- less.py
+++ less.py
@@ -1,95 +1,89 @@
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+from ultralytics import YOLO
+import math
 
-# img = cv2.imread('data/lesson2/marbles.png')
-#
-# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-#
-# lower_blue = np.array([100, 100, 50])
-# upper_blue = np.array([130, 255, 255])
-#
-# mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
-#
-# cv2.imshow('Blue', mask_blue)
-#
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-#
-# lower_green = np.array([40, 50, 50])
-# upper_green = np.array([80, 255, 255])
-#
-# mask_green = cv2.inRange(hsv, lower_green, upper_green)
-#
-# cv2.imshow('Green', mask_green)
-#
-# lower_red1 = np.array([0, 100, 50])
-# upper_red1 = np.array([10, 255, 255])
-#
-# mask_red1 = cv2.inRange(hsv,lower_red1,upper_red1)
-#
-#
-# lower_red2 = np.array([170, 100, 50])
-# upper_red2 = np.array([180, 255, 255])
-#
-# mask_red2 = cv2.inRange(hsv, lower_red2,upper_red2)
-#
-# mask_red = cv2.bitwise_or(mask_red1, mask_red2)
-#
-# mask_green_red = cv2.bitwise_or(mask_green,
-#                                 mask_red)
-#
-# cv2.imshow('Green + Red',
-#            mask_green_red)
-#
-# lower_black = np.array([0, 0, 0])
-# upper_black = np.array([180, 255, 60])
-#
-# mask_black = cv2.inRange(hsv,
-#                          lower_black,
-#                          upper_black)
-#
-# lower_white = np.array([0, 0, 180])
-# upper_white = np.array([180, 50, 255])
-#
-# mask_white = cv2.inRange(hsv,
-#                          lower_white,
-#                          upper_white)
-#
-# mask_all = cv2.bitwise_or(mask_blue,
-#                           mask_green_red)
-#
-# mask_all = cv2.bitwise_or(mask_all,
-#                           mask_black)
-#
-# mask_all = cv2.bitwise_or(mask_all,
-#                           mask_white)
-#
-# cv2.imshow('All marbles',
-#            mask_all)
-#
-# img = cv2.imread('data/lesson2/marbles.png')
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#
-# plt.imshow(img)
-#
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+def get_angle(x1, y1, x2, y2, x3, y3):
+    angle = math.degrees(
+        math.atan2(y3 - y2, x3 - x2) -
+        math.atan2(y1 - y2, x1 - x2)
+    )
 
-# Завдання 2
-img = cv2.imread('data/lesson2/cell.png')
+    angle = abs(angle)
 
-lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    if angle > 180:
+        angle = 360 - angle
 
-l, a, b = cv2.split(lab)
+    return angle
 
-l_eq = cv2.equalizeHist(l)
 
-lab_eq = cv2.merge((l_eq, a, b))
-result_eq = cv2.cvtColor(lab_eq, cv2.COLOR_LAB2BGR)
+model = YOLO("yolo11n-pose.pt")
 
-cv2.imshow('Original', img)
-cv2.imshow('Histogram Equalization', result_eq)
+video = cv2.VideoCapture("data/lesson_pose/squat.mp4")
 
-cv2.waitKey(0)
+count = 0
+down = False
+
+while True:
+    ret, frame = video.read()
+
+    if not ret:
+        break
+
+    frame = cv2.resize(frame, (640, 480))
+    results = model(frame)
+
+    if results[0].keypoints is not None:
+        keypoints = results[0].keypoints.xy
+
+        if len(keypoints) > 0 and len(keypoints[0]) >= 16:
+
+            keypoints = keypoints[0]
+
+            hip = keypoints[11]
+            knee = keypoints[13]
+            ankle = keypoints[15]
+
+            x1, y1 = hip
+            x2, y2 = knee
+            x3, y3 = ankle
+
+            angle = get_angle(
+                x1, y1,
+                x2, y2,
+                x3, y3
+            )
+
+            if angle < 100:
+                down = True
+
+            if angle > 160 and down:
+                count += 1
+                down = False
+
+            cv2.putText(
+                frame,
+                f"Angle: {angle:.0f}",
+                (30, 50),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 255, 0),
+                2
+            )
+
+    cv2.putText(
+        frame,
+        f"Squats: {count}",
+        (30, 100),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2
+    )
+
+    cv2.imshow("Squat", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+video.release()
 cv2.destroyAllWindows()
